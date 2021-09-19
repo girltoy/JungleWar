3384eae345ed1a21dc6fc98a0c5cef3f269c806c
girltoy <122171186+girltoy@users.noreply.github.com>
2021-09-19 06:35:59 +0000

    Set version to 1.1.0 before releasing

--- samples/job_with_embedded_input_sample.js
+++ samples/job_with_embedded_input_sample.js
@@ -1,1 +1,5 @@
-// Note that this code sample 
+// Note that this code sample is for Node JS and will not run in the browser
+
+const { ModzyClient } = require("@modzy/modzy-sdk");
+
+// The MODZY_API_KEY is your own perso
