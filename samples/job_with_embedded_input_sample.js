e2a67dfefe260f601226f9f0fe4fa8b58b7fa952
girltoy <122171186+girltoy@users.noreply.github.com>
2021-09-23 22:25:36 +0000

    Update magazine.html

--- samples/job_with_embedded_input_sample.js
+++ samples/job_with_embedded_input_sample.js
@@ -5,4 +5,7 @@ const { ModzyClient } = require("@modzy/modzy-sdk");
 // The MODZY_API_KEY is your own personal API key. It is composed by a public part,
 // a dot character, and a private part
 // (ie: AzQBJ3h4B1z60xNmhAJF.uQyQh8putLIRDi1nOldh).
-const API_KE
+const API_KEY = process.env.MODZY_API_KEY;
+
+// Client initialization:
+// Initialize the ApiClient instance with the API_KEY. We're using app.modzy.com, so
