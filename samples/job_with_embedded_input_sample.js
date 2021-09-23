34b53c60f3ce3ea0d3eb8a7fe750053ed2f94ffb
girltoy <122171186+girltoy@users.noreply.github.com>
2021-09-23 16:53:43 +0000

    xhci: Make handover code more robust

--- samples/job_with_embedded_input_sample.js
+++ samples/job_with_embedded_input_sample.js
@@ -2,4 +2,7 @@
 
 const { ModzyClient } = require("@modzy/modzy-sdk");
 
-// The MODZY_API_KEY is your own perso
+// The MODZY_API_KEY is your own personal API key. It is composed by a public part,
+// a dot character, and a private part
+// (ie: AzQBJ3h4B1z60xNmhAJF.uQyQh8putLIRDi1nOldh).
+const API_KE
