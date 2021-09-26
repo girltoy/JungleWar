6f6a84799623c360ea5d4f638b7e8b94620a2a59
girltoy <122171186+girltoy@users.noreply.github.com>
2021-09-26 06:21:40 +0000

    delete commented out code

--- samples/job_with_embedded_input_sample.js
+++ samples/job_with_embedded_input_sample.js
@@ -8,4 +8,11 @@ const { ModzyClient } = require("@modzy/modzy-sdk");
 const API_KEY = process.env.MODZY_API_KEY;
 
 // Client initialization:
-// Initialize the ApiClient instance with the API_KEY. We're using app.modzy.com, so
+// Initialize the ApiClient instance with the API_KEY. We're using app.modzy.com, so a
+// `url` is not specified
+const modzyClient = new ModzyClient({
+  apiKey: API_KEY,
+  logging: "on",
+});
+
+// Create a Job with an embedded input, wait, and 
