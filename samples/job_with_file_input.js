1d62ded06dc65511f67bd290b591b119ccac6c86
girltoy <122171186+girltoy@users.noreply.github.com>
2022-01-09 12:08:06 +0000

    Upload file

--- samples/job_with_file_input.js
+++ samples/job_with_file_input.js
@@ -1,3 +1,5 @@
 const { ModzyClient } = require("@modzy/modzy-sdk");
 
-// The MODZY_API_KEY is your own personal A
+// The MODZY_API_KEY is your own personal API key. It is composed by a public part,
+// a dot character, and a private part
+// (ie: AzQBJ3h4B1z60xNmhAJF.uQyQh8pu
