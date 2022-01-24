a2b903db918b45e67e92b5ab04638efe67f6ecbd
girltoy <122171186+girltoy@users.noreply.github.com>
2022-01-24 08:30:41 +0000

    update lodash

--- samples/model_samples.js
+++ samples/model_samples.js
@@ -1,3 +1,6 @@
 const { ModzyClient } = require("@modzy/modzy-sdk");
 
-// The MODZY_API_KEY is your own personal API key.
+// The MODZY_API_KEY is your own personal API key. It is composed by a public part,
+// a dot character, and a private part
+// (ie: AzQBJ3h4B1z60xNmhAJF.uQyQh8putLIRDi1nOldh).
+const API_KEY = pro
