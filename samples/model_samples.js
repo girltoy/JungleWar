ece6079c9f41fc179a0b62562c6ec9c50b741e33
girltoy <122171186+girltoy@users.noreply.github.com>
2022-02-10 18:16:07 +0000

    README [skip ci]

--- samples/model_samples.js
+++ samples/model_samples.js
@@ -3,4 +3,7 @@ const { ModzyClient } = require("@modzy/modzy-sdk");
 // The MODZY_API_KEY is your own personal API key. It is composed by a public part,
 // a dot character, and a private part
 // (ie: AzQBJ3h4B1z60xNmhAJF.uQyQh8putLIRDi1nOldh).
-const API_KEY = pro
+const API_KEY = process.env.MODZY_API_KEY;
+
+// Client initialization:
+// Initialize the ApiClient instance with the API_KEY. We're using app.modzy.com, so a
