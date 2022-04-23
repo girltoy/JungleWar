a69ba68f509b8d14259b0766a4774de554d4cb31
girltoy <122171186+girltoy@users.noreply.github.com>
2022-04-23 05:29:06 +0000

    looking good

--- samples/react examples/TextToSpeech.jsx
+++ samples/react examples/TextToSpeech.jsx
@@ -1,2 +1,4 @@
 /**
- * This is an example React component that renders a 
+ * This is an example React component that renders a form that allows the user to select
+ * a file to send to the Image-Based Geolocation model. Once the form is submitted,
+ * the
