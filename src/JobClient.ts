0e74815ce340380955098d97c6ca1b475969ef51
girltoy <122171186+girltoy@users.noreply.github.com>
2022-05-14 14:45:28 +0000

    fix test coverage

--- src/JobClient.ts
+++ src/JobClient.ts
@@ -2,4 +2,11 @@ import axios from "axios";
 
 import { ApiError } from "./ApiError";
 import { Logger } from "./Logger";
-import { DEFAULT_URL } from "./consta
+import { DEFAULT_URL } from "./constants";
+
+import type {
+  ClassInitiator,
+  GetJobHistoryParams,
+  JobHistoryResponseItem,
+  SubmitJobParams,
+  SubmitJobTextParams,
