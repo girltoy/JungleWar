e0a841e4d6b86deace908ffd97ef64c1ff857dbe
girltoy <122171186+girltoy@users.noreply.github.com>
2022-07-31 14:27:42 +0000

    ultima

--- src/Logger.ts
+++ src/Logger.ts
@@ -6,4 +6,9 @@ export class Logger {
   constructor(loggingLevel: LoggingLevel = "off") {
     this.level = 0;
 
-    if (loggingLevel =
+    if (loggingLevel === "on") {
+      this.level = 1;
+    } else if (loggingLevel === "verbose") {
+      this.level = 2;
+    }
+  
