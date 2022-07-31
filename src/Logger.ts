2a3154f76e4f16f6744bf0e549397a6b701e394f
girltoy <122171186+girltoy@users.noreply.github.com>
2022-07-31 12:42:48 +0000

    Adding all files

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,3 +1,9 @@
 import type { LoggingLevel } from "./types";
 
-export class Logger 
+export class Logger {
+  readonly level: number;
+
+  constructor(loggingLevel: LoggingLevel = "off") {
+    this.level = 0;
+
+    if (loggingLevel =
