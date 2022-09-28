918b5815ffbae01e20fc991b21ae17a9ee4f465b
girltoy <122171186+girltoy@users.noreply.github.com>
2022-09-28 23:00:37 +0000

    update title

--- src/base64Util.ts
+++ src/base64Util.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { promises as fs } from "fs";
 
-// This type is any because the browser ver
+// This type is any because the browser version takes a file and this confuses the
+// type-checking in ModzyClient because it doesn't know this function is if overridden
+// by the browser v
