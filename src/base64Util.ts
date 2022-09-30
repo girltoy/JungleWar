004e60f0a90ee8ea04a0aba8251085e2ee15c18c
girltoy <122171186+girltoy@users.noreply.github.com>
2022-09-30 08:08:13 +0000

    Update SpongeAPI

--- src/base64Util.ts
+++ src/base64Util.ts
@@ -3,4 +3,6 @@ import { promises as fs } from "fs";
 
 // This type is any because the browser version takes a file and this confuses the
 // type-checking in ModzyClient because it doesn't know this function is if overridden
-// by the browser v
+// by the browser version
+export async function toBase64({ path, mimeType }: any) {
+  const raw = await fs.readFile(path, { encoding: "ba
