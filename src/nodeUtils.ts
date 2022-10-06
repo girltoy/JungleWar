f717015ab9ca3980156e6f5425671c9472c5d937
girltoy <122171186+girltoy@users.noreply.github.com>
2022-10-06 07:43:44 +0000

    b43: Fix unload oops if firmware is not available

--- src/nodeUtils.ts
+++ src/nodeUtils.ts
@@ -5,4 +5,9 @@ const SIZE_PATTERN = /^(\d+(\.\d+)?)([a-zA-Z]{0,2})$/;
 const DataUnit = {
   i: 1, // BYTES
   K: 1000, // KILOBYTES
-  M: 1000
+  M: 1000 * 1000, // MEGABYTES
+  G: 1000 * 1000 * 1000, // GIGABYTES
+  T: 1000 * 1000 * 1000 * 1000, // TERABYTES
+  Ki: 1024,
+  Mi: 1024 * 1024,
+  Gi: 
